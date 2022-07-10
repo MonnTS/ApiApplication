5c91c942b881bee377c53f89d39db8e8da2afdca
Daniel <zujev.daniel20319@gmail.com>
2022-07-10 14:06:36 +0200

    Implemented GET/GET by Id/POST/PUT/DELETE

--- RestWebApp.API/Controllers/CarController.cs
+++ RestWebApp.API/Controllers/CarController.cs
@@ -1,5 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using RestWebApp.Contracts;
 using RestWebApp.Entities;
 using RestWebApp.Entities.Models;
@@ -10,7 +9,7 @@ namespace RestWebApp.API.Controllers;
 [Route("api/[controller]")]
 public class CarController : ControllerBase
 {
-    private IRepositoryWrapper _repoWrapper;
+    private readonly IRepositoryWrapper _repoWrapper;
 
     public CarController(IRepositoryWrapper repoWrapper, RepositoryContext context)
     {
@@ -18,5 +17,75 @@ public class CarController : ControllerBase
         context.Database.EnsureCreated();
     }
     
+    [HttpGet]
+    public IActionResult GetAllCars()
+    {
+        var cars = _repoWrapper.Cars.GetAll();
+        return Ok(cars);
+    }
     
+    [HttpGet("{id:int}")]
+    public IActionResult GetCarById(int id)
+    {
+        var car = _repoWrapper.Cars.GetByCondition(x => x.Id == id).FirstOrDefault();
+        
+        if(car == null)
+        {
+            return NotFound($"Car with this id {car.Id} not found");
+        }
+        
+        return Ok(car);
+    }
+    
+    [HttpPost]
+    public IActionResult CreateCar([FromBody] Car car)
+    {
+        if(car == null)
+        {
+            return BadRequest($"Car with id {car.Id} was not found");
+        }
+        
+        _repoWrapper.Cars.Create(car);
+        _repoWrapper.Save();
+        return Ok(car);
+    }
+    
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateCar(int id, [FromBody] Car car)
+    {
+        if(car == null || car.Id != id)
+        {
+            return BadRequest("Object car is null or id is not equal to id in url");
+        }
+        
+        var carToUpdate = _repoWrapper.Cars.GetByCondition(x => x.Id == id).FirstOrDefault();
+        
+        if(carToUpdate == null)
+        {
+            return NotFound($"Car with id {id} not found");
+        }
+            
+        carToUpdate.Description = car.Description;
+        carToUpdate.Brand = car.Brand;
+        carToUpdate.Date = car.Date;
+        
+        _repoWrapper.Cars.Update(carToUpdate);
+        _repoWrapper.Save();
+        return Ok(carToUpdate);
+    }
+    
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteCar(int id)
+    {
+        var car = _repoWrapper.Cars.GetByCondition(x => x.Id == id).FirstOrDefault();
+        
+        if (car == null)
+        {
+            return NotFound($"Car with id {id} not found");
+        }
+        
+        _repoWrapper.Cars.Delete(car);
+        _repoWrapper.Save();
+        return NoContent();
+    }
 }
--- RestWebApp.API/Controllers/UserController.cs
+++ RestWebApp.API/Controllers/UserController.cs
@@ -9,13 +9,83 @@ namespace RestWebApp.API.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    private IRepositoryWrapper _repo;
+    private readonly IRepositoryWrapper _repoWrapper;
     
-    public UserController(IRepositoryWrapper repo, RepositoryContext context)
+    public UserController(IRepositoryWrapper repoWrapper, RepositoryContext context)
     {
-        _repo = repo;
+        _repoWrapper = repoWrapper;
         context.Database.EnsureCreated();
     }
     
+    [HttpGet]
+    public IActionResult GetAllUsers()
+    {
+        var users = _repoWrapper.Users.GetAll();
+        return Ok(users);
+    }
     
+    [HttpGet("{id:int}")]
+    public IActionResult GetUserById(int id)
+    {
+        var user = _repoWrapper.Users.GetByCondition(x => x.Id == id).FirstOrDefault();
+        
+        if(user == null)
+        {
+            return NotFound($"User with this id {user.Id} not found");
+        }
+        
+        return Ok(user);
+    }
+    
+    [HttpPost]
+    public IActionResult CreateUser([FromBody] User user)
+    {
+        if(user == null)
+        {
+            return BadRequest($"User with id {user.Id} was not found");
+        }
+        
+        _repoWrapper.Users.Create(user);
+        _repoWrapper.Save();
+        return Ok(user);
+    }
+    
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateUser(int id, [FromBody] User user)
+    {
+        if(user == null || user.Id != id)
+        {
+            return BadRequest("Object user is null or id is not equal to id in url");
+        }
+        
+        var userToUpdate = _repoWrapper.Users.GetByCondition(x => x.Id == id).FirstOrDefault();
+        
+        if(userToUpdate == null)
+        {
+            return NotFound($"Car with id {id} not found");
+        }
+        
+        userToUpdate.Name = user.Name;
+        userToUpdate.Password = user.Password;
+        userToUpdate.Role = user.Role;
+        
+        _repoWrapper.Users.Update(userToUpdate);
+        _repoWrapper.Save();
+        return Ok(userToUpdate);
+    }
+    
+    [HttpDelete("{id:int}")]
+    public IActionResult UserCar(int id)
+    {
+        var user = _repoWrapper.Users.GetByCondition(x => x.Id == id).FirstOrDefault();
+        
+        if (user == null)
+        {
+            return NotFound($"Car with id {id} not found");
+        }
+        
+        _repoWrapper.Users.Delete(user);
+        _repoWrapper.Save();
+        return NoContent();
+    }
 }
