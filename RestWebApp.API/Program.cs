3a615af7d5fd981fba389c37e429a5b4c1334c76
Daniel <zujev.daniel20319@gmail.com>
2022-07-10 18:15:44 +0200

    Refactor class

--- RestWebApp.API/Program.cs
+++ RestWebApp.API/Program.cs
@@ -3,13 +3,13 @@ namespace RestWebApp.API;
 internal class Program
 {
     private static void Main(string[] args)
-    => CreateHostBuilder(args).Build().Run();
-    
-    private static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
-}
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
 
+    private static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+}
