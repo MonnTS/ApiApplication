4d9c25a0479b38c6aefe8113bef856f03dcb50bb
Daniel <zujev.daniel20319@gmail.com>
2022-07-10 17:21:08 +0200

    Refactor class

--- RestWebApp.Repository/RepositoryWrapper.cs
+++ RestWebApp.Repository/RepositoryWrapper.cs
@@ -5,9 +5,9 @@ namespace RestWebApp.Repository;
 
 public class RepositoryWrapper : IRepositoryWrapper
 {
-    private RepositoryContext _context;
-    private ICarRepository _car;
-    private IUserRepository _user;
+    private readonly RepositoryContext _context;
+    private ICarRepository? _car;
+    private IUserRepository? _user;
 
     public ICarRepository Cars => _car ??= new CarRepository(_context);
     public IUserRepository Users => _user ??= new UserRepository(_context);
