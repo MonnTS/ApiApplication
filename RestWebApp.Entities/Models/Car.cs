d4d5e7b515dcdf536e5b02199b6102e745e472b1
Daniel <zujev.daniel20319@gmail.com>
2022-07-10 17:19:43 +0200

    Updated models

--- RestWebApp.Entities/Models/Car.cs
+++ RestWebApp.Entities/Models/Car.cs
@@ -5,15 +5,12 @@ namespace RestWebApp.Entities.Models;
 public class Car
 {
     private DateTime _dateTime;
-    
-    [Required]
-    public int Id { get; set; }
-    
-    [Required]
-    public string Brand {get; set; }
-    
-    [Required]
-    public string Description {get; set; }
+
+    [Required] public int Id { get; set; }
+
+    [Required] public string Brand { get; set; }
+
+    [Required] public string Description { get; set; }
 
     [Required]
     public string Date
@@ -21,17 +18,11 @@ public class Car
         get => _dateTime.ToString("dd-MM-yyyy");
         set
         {
-            if (value == _dateTime.ToString("dd-MM-yyyy"))
-            {
-                return;
-            }
-            
+            if (value == _dateTime.ToString("dd-MM-yyyy")) return;
+
             var correctDateTime = DateTime.TryParse(value, out _dateTime);
 
-            if (!correctDateTime)
-            {
-                _dateTime = DateTime.Now;
-            }
+            if (!correctDateTime) _dateTime = DateTime.Now;
         }
     }
 }
--- RestWebApp.Entities/Models/User.cs
+++ RestWebApp.Entities/Models/User.cs
@@ -4,16 +4,12 @@ namespace RestWebApp.Entities.Models;
 
 public class User
 {
-    [Key]
-    [Required]
-    public int Id { get; set; }
-    
-    [Required]    
-    public string Name { get; set; }
-    
+    [Key] [Required] public int Id { get; init; }
+
+    [Required] public string Name { get; set; }
+
     [MinLength(8, ErrorMessage = "Password must be at least 8 characters long")]
     public string Password { get; set; }
-    
-    [Required]
-    public string Role { get; set; }
+
+    [Required] public string Role { get; set; }
 }
