33f8aba58d9a16fa09e5722a3cc50882090baf0b
Monn <zujev.daniel20319@gmail.com>
2023-07-17 23:41:22 +0200

    refactor: Clear formatting

--- RestWebApp.Entities/RepositoryContext.cs
+++ RestWebApp.Entities/RepositoryContext.cs
@@ -3,13 +3,13 @@ using RestWebApp.Entities.Models;
 
 namespace RestWebApp.Entities;
 
-public class RepositoryContext: DbContext
+public class RepositoryContext : DbContext
 {
     public RepositoryContext(DbContextOptions<RepositoryContext> options)
         : base(options)
     {
     }
-        
+
     public DbSet<Car> Cars { get; set; } = null!;
     public DbSet<User> Users { get; set; } = null!;
 
@@ -17,8 +17,8 @@ public class RepositoryContext: DbContext
     {
         modelBuilder.Entity<User>().HasData
         (
-            new User {Id = 1, Name = "admin", Password = "AAAAAAAAA", Role = "Admin"}, 
-            new User {Id = 2, Name = "test", Password = "test", Role = "User"}
+            new User { Id = 1, Name = "admin", Password = "AAAAAAAAA", Role = "Admin" },
+            new User { Id = 2, Name = "test", Password = "test", Role = "User" }
         );
 
         modelBuilder.Entity<Car>().HasData(
